34e57d30d6d3d25d7812c37e8734e22814887cd7
thefrustrateddev <the.frustrated.dev01@gmail.com>
2021-10-30 17:00:30 +0530

    initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="header">
+    <div class="logo section">
+      Logo
+    </div>
+    <div class="option section">
+      <div class="Projects">
+        Projects
+      </div>
+      <div class="Blogs">
+        Blogs
+      </div>
+      <div class="Contact me">
+        Contact me
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  setup() {
+
+  },
+});
+</script>
+<style lang="scss">
+.header{
+  background: black;
+  color: white;
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 90px;
+
+  .section{
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    padding: 20px;
+  }
+  .logo{
+
+    flex: 0 0 100px;
+  }
+  .option{
+justify-content: flex-end;
+    background: red;
+    flex: 1;
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+  },
+};
+</script>
+<style lang="scss">
+.home{
+  background: blue;
+  height: 100vh;
+  width: 100vw;
+}
+</style>
