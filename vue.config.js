e5dac7249c49c93a7cfe8d1c23188fea309c50fb
thefrustrateddev <the.frustrated.dev01@gmail.com>
2021-10-31 13:03:58 +0530

    title change

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,15 @@ const bootstrapSassAbstractsImports = require('vue-cli-plugin-bootstrap-vue/sass
 
 module.exports = {
   publicPath: '/portfolio/',
+  chainWebpack: (config) => {
+    config
+      .plugin('html')
+      .tap((args) => {
+        // eslint-disable-next-line no-param-reassign
+        args[0].title = 'The Frustrated Dev';
+        return args;
+      });
+  },
   css: {
     loaderOptions: {
       sass: {
