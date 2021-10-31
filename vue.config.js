89381354c5d5bf490cc392a358bd4665b1f7697e
thefrustrateddev <the.frustrated.dev01@gmail.com>
2021-10-31 12:35:38 +0530

    test action#1

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const bootstrapSassAbstractsImports = require('vue-cli-plugin-bootstrap-vue/sassAbstractsImports');
 
 module.exports = {
+  publicPath: '/portfolio/',
   css: {
     loaderOptions: {
       sass: {
